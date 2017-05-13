3f0836dc353c5d4c78b3e1dabf59772069ecea9c
Roman <bolroman@gmail.com>
2017-05-13 21:17:10 +0300

    start point

---
+++ BudgetManager/BudgetManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BudgetManager
+//
+//  Created by Roman on 13/05/2017.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BudgetManager/BudgetManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BudgetManager
+//
+//  Created by Roman on 13/05/2017.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
